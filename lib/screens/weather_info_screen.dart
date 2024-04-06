5f79c657fa3644c977abef6e3f7ba029e5a38fde
Akhil184 <akhilteli55@gmail.com>
2024-04-06 13:45:38 +0530

    design change

--- lib/screens/weather_info_screen.dart
+++ lib/screens/weather_info_screen.dart
@@ -26,32 +26,57 @@ class _WeatherScreenState extends State<WeatherScreen> {
           centerTitle:true,
           backgroundColor:Colors.green,
         ),
-        body:FutureBuilder<Weathers>(
+        body: FutureBuilder<Weathers>(
     future: futureAlbums,
     builder: (context, snapshot) {
     if (snapshot.hasData) {
     return  Padding(padding: new EdgeInsets.all(10.0),
       child:Column(
       children: [
-        if(snapshot.data!.weather![0].description=='clear sky')
-          Image.asset('assets/images/clear_sky.jpeg',fit:BoxFit.cover,height:650,),
-        if(snapshot.data!.weather![0].description=='haze')
-          Image.asset('assets/images/haze.jpeg',fit:BoxFit.cover,height:650,),
-        if(snapshot.data!.weather![0].description=='scattered clouds')
-          Image.asset('assets/images/scatted_cloud.webp',fit:BoxFit.cover,height:650),
-        if(snapshot.data!.weather![0].description=='overcast clouds')
-          Image.asset('assets/images/overcast.webp',fit:BoxFit.cover,height:650,),
-        if(snapshot.data!.weather![0].description=='mist')
-          Image.asset('assets/images/mist.jpeg',fit:BoxFit.cover,height:650,),
-        if(snapshot.data!.weather![0].description=='broken clouds')
-          Image.asset('assets/images/scattred_cloud.webp',fit:BoxFit.cover,height:650,),
-        if(snapshot.data!.weather![0].description=='few clouds')
-          Image.asset('assets/images/few_clouds.jpeg',fit:BoxFit.cover,height:650,),
-      Padding(padding: new EdgeInsets.all(0.0),
-      child:Text(snapshot.data!.weather![0].description ?? '',style:TextStyle(fontSize:20),),
+        Card(
+          color:Colors.lightBlue[200],
+                child:Column(
+                  mainAxisAlignment:MainAxisAlignment.start,
+                  crossAxisAlignment:CrossAxisAlignment.start,
+                  children: [
+                    Padding(padding: new EdgeInsets.all(10.0),
+                  child:Text('Hourly Forecast'),
       ),
+                    Padding(padding: new EdgeInsets.all(10.0),
+                  child:Row(
+                    mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Now',style:TextStyle(color:Colors.white)),
+                        Text('7 Am',style:TextStyle(color:Colors.white)),
+                          Text('8 Am ',style:TextStyle(color:Colors.white)),
+                      Text('9 Am ',style:TextStyle(color:Colors.white)),
+                      Text('10 Am ',style:TextStyle(color:Colors.white)),
 
-      ]
+                    ],
+                  ),
+                ),
+      ],
+        ),
+        //     if(snapshot.data!.weather![0].description=='clear sky')
+        //   Image.asset('assets/images/clear_sky.jpeg',fit:BoxFit.cover,height:650,),
+        // if(snapshot.data!.weather![0].description=='haze')
+        //   Image.asset('assets/images/haze.jpeg',fit:BoxFit.cover,height:650,),
+        // if(snapshot.data!.weather![0].description=='scattered clouds')
+        //   Image.asset('assets/images/scatted_cloud.webp',fit:BoxFit.cover,height:650),
+        // if(snapshot.data!.weather![0].description=='overcast clouds')
+        //   Image.asset('assets/images/overcast.webp',fit:BoxFit.cover,height:650,),
+        // if(snapshot.data!.weather![0].description=='mist')
+        //   Image.asset('assets/images/mist.jpeg',fit:BoxFit.cover,height:650,),
+        // if(snapshot.data!.weather![0].description=='broken clouds')
+        //   Image.asset('assets/images/scattred_cloud.webp',fit:BoxFit.cover,height:650,),
+        // if(snapshot.data!.weather![0].description=='few clouds')
+        //   Image.asset('assets/images/few_clouds.jpeg',fit:BoxFit.cover,height:650,),
+      // Padding(padding: new EdgeInsets.all(0.0),
+      // child:Text(snapshot.data!.weather![0].description ?? '',style:TextStyle(fontSize:20),),
+      // ),
+
+      ),
+      ],
       ),
       );
     } else if (snapshot.hasError) {
