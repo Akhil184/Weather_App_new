150d18a7398c275d8be8b821d501c5e090b08bbc
Akhil184 <akhilteli55@gmail.com>
2024-04-06 20:12:06 +0530

    implement rain animation

--- lib/screens/weather_info_screen.dart
+++ lib/screens/weather_info_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:newton_particles/newton_particles.dart';
 import '../http_call/get_data.dart';
 import '../models/get_data_model.dart';
 
@@ -21,42 +21,50 @@ class _WeatherScreenState extends State<WeatherScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor:Colors.blueGrey,
         appBar:AppBar(
           title:Text('Weather App'),
           centerTitle:true,
           backgroundColor:Colors.green,
         ),
-        body: FutureBuilder<Weathers>(
-    future: futureAlbums,
-    builder: (context, snapshot) {
-    if (snapshot.hasData) {
-    return  Padding(padding: new EdgeInsets.all(10.0),
-      child:Column(
-      children: [
-        Card(
-          color:Colors.lightBlue[200],
-                child:Column(
-                  mainAxisAlignment:MainAxisAlignment.start,
-                  crossAxisAlignment:CrossAxisAlignment.start,
-                  children: [
-                    Padding(padding: new EdgeInsets.all(10.0),
-                  child:Text('Hourly Forecast'),
-      ),
-                    Padding(padding: new EdgeInsets.all(10.0),
-                  child:Row(
-                    mainAxisAlignment:MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Now',style:TextStyle(color:Colors.white)),
-                        Text('7 Am',style:TextStyle(color:Colors.white)),
-                          Text('8 Am ',style:TextStyle(color:Colors.white)),
-                      Text('9 Am ',style:TextStyle(color:Colors.white)),
-                      Text('10 Am ',style:TextStyle(color:Colors.white)),
-
-                    ],
-                  ),
-                ),
-      ],
+        body:Newton(
+          // Add any kind of effects to your UI
+          // For example:
+          activeEffects: [
+            RainEffect(
+              particleConfiguration: ParticleConfiguration(
+                shape: CircleShape(),
+                size: const Size(5, 5),
+                color: const SingleParticleColor(color: Colors.black),
+              ),
+              effectConfiguration: const EffectConfiguration(),
+            )
+          ],
         ),
+      //   Card(
+      //     color:Colors.lightBlue[200],
+      //           child:Column(
+      //             mainAxisAlignment:MainAxisAlignment.start,
+      //             crossAxisAlignment:CrossAxisAlignment.start,
+      //             children: [
+      //               Padding(padding: new EdgeInsets.all(10.0),
+      //             child:Text('Hourly Forecast'),
+      // ),
+      //               Padding(padding: new EdgeInsets.all(10.0),
+      //             child:Row(
+      //               mainAxisAlignment:MainAxisAlignment.spaceBetween,
+      //               children: [
+      //                 Text('Temp',style:TextStyle(color:Colors.white)),
+      //                   // Text({{snapshot.}},style:TextStyle(color:Colors.white)),
+      //                     Text('8 Am ',style:TextStyle(color:Colors.white)),
+      //                 Text('9 Am ',style:TextStyle(color:Colors.white)),
+      //                 Text('10 Am ',style:TextStyle(color:Colors.white)),
+      //
+      //               ],
+      //             ),
+      //           ),
+      // ],
+      //   ),
         //     if(snapshot.data!.weather![0].description=='clear sky')
         //   Image.asset('assets/images/clear_sky.jpeg',fit:BoxFit.cover,height:650,),
         // if(snapshot.data!.weather![0].description=='haze')
@@ -75,18 +83,18 @@ class _WeatherScreenState extends State<WeatherScreen> {
       // child:Text(snapshot.data!.weather![0].description ?? '',style:TextStyle(fontSize:20),),
       // ),
 
-      ),
-      ],
-      ),
-      );
-    } else if (snapshot.hasError) {
-    return Text('${snapshot.error}');
-    }
-
-    // By default, show a loading spinner.
-    return const CircularProgressIndicator();
-    },
-    ),
+    //   ),
+    //   ],
+    //   ),
+    //   );
+    // } else if (snapshot.hasError) {
+    // return Text('${snapshot.error}');
+    // }
+    //
+    // // By default, show a loading spinner.
+    // return const CircularProgressIndicator();
+    // },
+    // ),
     );
   }
 }
